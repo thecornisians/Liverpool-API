50e79bd686c579d686bf61054a22cece22745b80
Cornelius Owusu-Ansah <thecornisians@gmail.com>
2022-06-08 16:48:26 +0100

    made changes to js file

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 document.querySelector('#submit').addEventListener('click' , addToDom)
 const value = document.querySelector('#playerName').value
 
-const playerImage = document.querySelector('player-image')
-const playerName = document.querySelector('player-name')
-const shirtNumber = document.querySelector('shirt-name')
-const nationality = document.querySelector('nationality')
-const assist = document.querySelector('assist')
-const goals = document.querySelector('goals')
+const playerImage = document.querySelector('.player-image')
+const playerName = document.querySelector('.player-name')
+const shirtNumber = document.querySelector('.shirt-name')
+const nationality = document.querySelector('.nationality')
+const assist = document.querySelector('.assist')
+const goals = document.querySelector('.goals')
 
 
 async function addToDom (){
